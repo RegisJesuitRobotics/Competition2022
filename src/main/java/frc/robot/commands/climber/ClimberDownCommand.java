2d90d148a148a8928211d54b4097bde80a53c784
Nicholas James Turner <turnerni25@student.regisjesuit.com>
2022-02-19 13:39:28 -0700

    Nick changes

---
+++ src/main/java/frc/robot/commands/climber/ClimberDownCommand.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+
+public class ClimberDownCommand extends CommandBase {
+  private final double PERCENT = -0.5;
+  private Climber climber = new Climber();
+  /** Creates a new ClimberDownCommand. */
+  public ClimberDownCommand(Climber climber) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(climber);
+    this.climber = climber;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    climber.ClimberLength(PERCENT);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    climber.ClimberRotation(0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
