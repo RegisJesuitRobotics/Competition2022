2361893d593d38acd4acddbc196a362ed3e70976
ohowe <ohowe@protonmail.com>
2022-02-22 16:09:15 -0700

    Fix bug

--- src/main/java/frc/robot/commands/shooter/RunShooterAndFeederCommand.java
+++ src/main/java/frc/robot/commands/shooter/RunShooterAndFeederCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.shooter;
 
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.feeder.RunFeederCommand;
 import frc.robot.subsystems.Feeder;
@@ -7,7 +8,7 @@ import frc.robot.subsystems.Shooter;
 
 public class RunShooterAndFeederCommand extends SequentialCommandGroup {
     public RunShooterAndFeederCommand(double shooterRPM, Shooter shooter, Feeder feeder) {
-        super(new RunShooterCommand(shooterRPM, shooter), new WaitForShooterWarmup(shooterRPM, shooter),
-                new RunFeederCommand(feeder));
+        super(new ParallelRaceGroup(new RunShooterCommand(shooterRPM, shooter),
+                new WaitForShooterWarmupCommand(shooterRPM, shooter)), new RunFeederCommand(feeder));
     }
 }
--- src/main/java/frc/robot/commands/shooter/WaitForShooterWarmupCommand.java
+++ src/main/java/frc/robot/commands/shooter/WaitForShooterWarmupCommand.java
@@ -3,8 +3,8 @@ package frc.robot.commands.shooter;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Shooter;
 
-public class WaitForShooterWarmup extends WaitUntilCommand {
-    public WaitForShooterWarmup(double targetRPM, Shooter shooter) {
+public class WaitForShooterWarmupCommand extends WaitUntilCommand {
+    public WaitForShooterWarmupCommand(double targetRPM, Shooter shooter) {
         super(() -> shooter.getShooterRPM() >= targetRPM);
     }
 }
