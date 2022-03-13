de6fdce0ce082e0ae62fde44937c84e7784c6bc6
ohowe <ohowe@protonmail.com>
2022-03-12 17:44:57 -0700

    Change to proximity for detecting ball

--- src/main/java/frc/robot/commands/feeder/WaitForNoBallDetectedCommand.java
+++ src/main/java/frc/robot/commands/feeder/WaitForNoBallDetectedCommand.java
@@ -2,11 +2,10 @@ package frc.robot.commands.feeder;
 
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Feeder.FeederSensorStatus;
 
 public class WaitForNoBallDetectedCommand extends WaitUntilCommand {
     public WaitForNoBallDetectedCommand(Feeder feeder) {
-        super(() -> feeder.getSensorStatus() == FeederSensorStatus.NOTHING);
+        super(() -> !feeder.isBallLoaded());
     }
 
 }
