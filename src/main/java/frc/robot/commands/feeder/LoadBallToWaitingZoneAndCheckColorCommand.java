09f93beca3fa6e5917465bb0230e580a1b3a9ed1
ohowe <ohowe@protonmail.com>
2022-03-21 14:23:49 -0600

    Add delay to assure that we have updated value
    Using 0.08 because it is 100ms update on color sensor, but 20ms update on WPILib. The sensor will also be able to see what color before it stops (in real time)

--- src/main/java/frc/robot/commands/feeder/LoadBallToWaitingZoneAndCheckColorCommand.java
+++ src/main/java/frc/robot/commands/feeder/LoadBallToWaitingZoneAndCheckColorCommand.java
@@ -3,13 +3,14 @@ package frc.robot.commands.feeder;
 import edu.wpi.first.wpilibj2.command.ProxyScheduleCommand;
 import edu.wpi.first.wpilibj2.command.ScheduleCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.intake.Spinners;
 
 public class LoadBallToWaitingZoneAndCheckColorCommand extends SequentialCommandGroup {
     public LoadBallToWaitingZoneAndCheckColorCommand(Feeder feeder, Shooter shooter, Spinners spinners) {
-        super(new ProxyScheduleCommand(new LoadBallToWaitingZoneCommand(feeder, spinners)),
+        super(new ProxyScheduleCommand(new LoadBallToWaitingZoneCommand(feeder, spinners)), new WaitCommand(0.08),
                 new ScheduleCommand(new ExpelIfBallWrongColorCommand(feeder, shooter, spinners)));
     }
 }
