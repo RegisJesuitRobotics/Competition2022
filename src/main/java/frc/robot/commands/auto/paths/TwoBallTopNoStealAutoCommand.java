061bf3b0faef5834f56b0e7b4d69f4cd2e19dc4e
ohowe <ohowe@protonmail.com>
2022-03-21 13:58:51 -0600

    Remove limelight references

--- src/main/java/frc/robot/commands/auto/paths/TwoBallTopNoStealAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/paths/TwoBallTopNoStealAutoCommand.java
@@ -6,12 +6,10 @@ import frc.robot.commands.auto.RunTrajectoryLoadingBallCommand;
 import frc.robot.commands.drive.RotateDriveCommand;
 import frc.robot.commands.intake.IntakeDeployCommand;
 import frc.robot.commands.intake.IntakeUnDeployCommand;
-import frc.robot.commands.limelight.LimeLightAlignCommand;
 import frc.robot.commands.shooter.ShooterAimStateCommand;
 import frc.robot.commands.shooter.TwoBallShootSequenceCommand;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.LimeLight;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Shooter.AimState;
 import frc.robot.subsystems.intake.Intake;
@@ -19,11 +17,11 @@ import frc.robot.subsystems.intake.Spinners;
 
 public class TwoBallTopNoStealAutoCommand extends SequentialCommandGroup {
     public TwoBallTopNoStealAutoCommand(DriveTrain driveTrain, Intake intake, Shooter shooter, Feeder feeder,
-            Spinners spinners, LimeLight limeLight) {
+            Spinners spinners) {
         super(new IntakeDeployCommand(intake),
                 new RunTrajectoryLoadingBallCommand("2BallTopA", driveTrain, intake, spinners, feeder),
                 new IntakeUnDeployCommand(intake), new ShooterAimStateCommand(AimState.FAR, shooter),
-                new RotateDriveCommand(170, driveTrain), new LimeLightAlignCommand(limeLight, driveTrain),
+                new RotateDriveCommand(170, driveTrain),
                 new TwoBallShootSequenceCommand(ShooterConstants.TWO_BALL_DISTANCE_RPM, feeder, shooter, spinners),
                 new ShooterAimStateCommand(AimState.CLOSE, shooter));
     }
