bea00a01e2b7e4a945bb0859ea11c75c57b827d5
ohowe <ohowe@protonmail.com>
2022-12-15 13:17:48 -0700

    Speed & rate limiter

--- src/main/java/frc/robot/commands/drive/TankishDriveCommand.java
+++ src/main/java/frc/robot/commands/drive/TankishDriveCommand.java
@@ -1,12 +1,21 @@
 package frc.robot.commands.drive;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.joysticks.PlaystationController;
 import frc.robot.subsystems.DriveTrain;
 
+import java.util.Map;
+
 public class TankishDriveCommand extends CommandBase {
     private final DriveTrain driveTrain;
     private final PlaystationController driverController;
+    private final NetworkTableEntry maxSpeedEntry = Shuffleboard.getTab("OutreachRaw").add("Max Speed", 0.8)
+            .withWidget(BuiltInWidgets.kNumberSlider).withProperties(Map.of("min", 0, "max", 1.0)).getEntry();
+    private final SlewRateLimiter xyRateLimiter = new SlewRateLimiter(5.0);
 
     public TankishDriveCommand(DriveTrain driveTrain, PlaystationController driverController) {
         this.driveTrain = driveTrain;
@@ -26,7 +35,7 @@ public class TankishDriveCommand extends CommandBase {
         double rightTrigger = driverController.rightTrigger.getAxis();
         double leftStickX = driverController.leftThumb.getXAxis();
 
-        double speed = rightTrigger - leftTrigger;
+        double speed = xyRateLimiter.calculate(rightTrigger - leftTrigger);
         double turn = 2 * leftStickX;
 
         double leftSpeed = speed;
@@ -36,7 +45,7 @@ public class TankishDriveCommand extends CommandBase {
         } else if (turn < 0) {
             leftSpeed += turn * speed;
         }
-        driveTrain.tankDrive(leftSpeed * 0.8, rightSpeed * 0.8);
+        driveTrain.tankDrive(leftSpeed * maxSpeedEntry.getDouble(0.8), rightSpeed * maxSpeedEntry.getDouble(0.8));
     }
 
     @Override
